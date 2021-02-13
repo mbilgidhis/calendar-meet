f666146a62a72c066a95a5f4da70f76579a098e9
Andi Baskoro <baskoro.ruliawan@gmail.com>
2021-02-13 14:19:39 +0700

    add default string length on app service prvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') !== 'local') {
             $url->forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
     }
 }
